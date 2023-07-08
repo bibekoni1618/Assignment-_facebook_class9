63872995a4e5d1aa9d96b53262df05567e02c250
ronjon <bibekoni1618@gmail.com>
2023-07-08 20:47:23 +0600

    facebook post timeline design and dynamic done

---
+++ src/data.js
@@ -0,0 +1,223 @@
+const posts = [
+  {
+    id: 1,
+    p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+    p_media: {
+      photo:
+        "https://www.elegantthemes.com/blog/wp-content/uploads/2023/05/Jasper-AI-Review-and-Guide.jpg",
+    },
+
+    like_count: 450,
+    comment_count: 100,
+    post_time: "10 min",
+    user: {
+      name: "Jasper AI",
+      photo:
+        "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+    },
+  },
+  {
+    id: 2,
+    p_content: `অনলাইনে কেনাকাটায় ভিসা কার্ড সেভ করে পেমেন্টে উপভোগ করুন সর্বোচ্চ ২০% পর্যন্ত ছাড়*। 
+                    বিস্তারিত জানতে ভিজিট করুন: https://visa.sslcommerz.com/
+                    *শর্ত প্রযোজ্য। 
+                    *অফার চলবে ৩০ জুন ২০২৩ পর্যন্ত।`,
+    p_media: {
+      photo:
+        "https://sslcommerz.com/wp-content/uploads/2023/02/Payment-gateway-in-Bangladesh.jpg",
+    },
+
+    like_count: 4.5 + "k",
+    comment_count: 200,
+    post_time: "10 min",
+    user: {
+      name: "SSL Commerz",
+      photo: "https://avatars.githubusercontent.com/u/19384040?s=280&v=4",
+    },
+  },
+  {
+    id: 3,
+    p_content: `"𝐆𝐨𝐨𝐠𝐥𝐞 𝐀𝐈 big update June 26, no registration required, 142 languages, works in all countries
+                 Smarter - Learn better - unlimited creative thinking
+                Free to use => https://sites.google.com/view/aipoweredfuture
+             In addition, the new update also supports marketing very well, supports creating feverish marketing content"`,
+    p_media: {
+      photo:
+        "https://nypost.com/wp-content/uploads/sites/2/2023/02/Gogole-stock-chart.jpg",
+    },
+
+    like_count: 450,
+    comment_count: 100,
+    post_time: "10 min",
+    user: {
+      name: "BARD AI",
+      photo:
+        "https://www.perfomatix.com/wp-content/uploads/2023/03/BARD-AI.png",
+    },
+  },
+  {
+    id: 4,
+    p_content: `My Workspace Setup🤩🤩`,
+    p_media: {
+      photo:
+        "https://raw.githubusercontent.com/Deveripon/my-photos/main/desk.jpg",
+    },
+
+    like_count: 40,
+    comment_count: 10,
+    post_time: "30 min",
+    user: {
+      name: "Shahadat Hussain Ripon",
+      photo:
+        "https://avatars.githubusercontent.com/u/93313948?s=400&u=3b7b0eae8fe6fefc90d310fc5bfd35ee7715db81&v=4",
+    },
+  },
+  {
+    id: 5,
+    p_content: `10 Killer React Animation Libraries.
+
+একটি Web-site কে নান্দনিক করার জন্য আমরা অনেক কিছু করে থাকি।
+বিভিন্ন ডিজাইন আমরা ফলো করি।
+
+একটি  Web-site কে সুন্দর করার 
+অনেক মাধ্যম থাকলেও তার মধ্যে একটি হল Animation ।
+তাই, আজকের এই  দশটি React এর Animation Libraries  আপনাদের অনেক কাজে দিবে
+
+Framer Motion
+Site Link : https://www.framer.com
+
+Pose
+Site Link : https://popmotion.io/pose/
+
+React Spring
+Site Link : https://www.react-spring.dev/
+
+React Reveal
+Site Link : https://www.react-reveal.com/
+
+React Motion
+Site Link : https://www.npmjs.com/package/react-motion
+
+Remotion
+Site Link : https://www.remotion.dev/
+
+React GSAP
+Site Link : https://greensock.com/react/
+
+React Awesome Reveal
+Site Link : https://react-awesome-reveal.morello.dev/
+
+Remotion 
+Site Link : https://www.remotion.dev/
+
+Lottie
+Site Link : https://airbnb.io/lottie/#/
+
+#react`,
+    p_media: {
+      photo:
+        "https://www.syncfusion.com/blogs/wp-content/uploads/2022/08/Top-7-React-Animation-Libraries-in-2022.png",
+    },
+
+    like_count: 40,
+    comment_count: 200,
+    post_time: "Just now",
+    user: {
+      name: "Shahadat Hussain Ripon",
+      photo:
+        "https://avatars.githubusercontent.com/u/93313948?s=400&u=3b7b0eae8fe6fefc90d310fc5bfd35ee7715db81&v=4",
+    },
+  },
+  {
+    id: 6,
+    p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+    p_media: {
+      photo:
+        "https://blog.hubspot.com/hs-fs/hubfs/Google%20Drive%20Integration/How%20Jasper.ai%20Writing%20Assistant%20Works%20%5B+%20Its%20Pros%20and%20Cons%5D-1.png?width=550&height=481&name=How%20Jasper.ai%20Writing%20Assistant%20Works%20%5B+%20Its%20Pros%20and%20Cons%5D-1.png",
+    },
+
+    like_count: 450,
+    comment_count: 100,
+    post_time: "3 min",
+    user: {
+      name: "Jasper AI",
+      photo:
+        "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+    },
+  },
+  {
+    id: 7,
+    p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+    p_media: {
+      photo:
+        "https://www.jagoanhosting.com/blog/wp-content/uploads/2023/05/jasper.png",
+    },
+
+    like_count: 450,
+    comment_count: 100,
+    post_time: "10 min",
+    user: {
+      name: "Jasper AI",
+      photo:
+        "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+    },
+  },
+  {
+    id: 8,
+    p_content: `পরশুই অধিনায়ক হিসেবে সিরিজ–পূর্ব সংবাদ সম্মেলন করেছেন। কাল প্রথম ওয়ানডেতে হারের পর স্বাভাবিকভাবেই সংবাদ সম্মেলনে আসেন দলের সেরা পারফরমার তাওহিদ হৃদয়। আর আজ তো বাংলাদেশ দলের অনুশীলনই নেই। সংবাদমাধ্যমের সামনেও আসার কথা নয় দলের কারও।
+
+		অথচ আজই কিনা চট্টগ্রামে হতে যাচ্ছে কৌতূহলজাগানিয়া এক সংবাদ সম্মেলন! আর সেটি ডেকেছেন খোদ ওয়ানডে অধিনায়ক তামিম ইকবাল। সেটাও বিসিবি বা দলের মাধ্যমে নয়, একান্ত ব্যক্তিগত উদ্যোগে।`,
+    p_media: {
+      photo:
+        "https://images.prothomalo.com/prothomalo-bangla%2F2023-07%2F8321f12e-dd4d-4df8-99da-0c8461b43022%2FTamim_meet_the_press_at_Ctg_____30.jpg?rect=0%2C0%2C3372%2C1897&auto=format%2Ccompress&fmt=webp&format=webp&w=900&dpr=1.3",
+    },
+
+    like_count: 450,
+    comment_count: 100,
+    post_time: "10 min",
+    user: {
+      name: "Prothom ALo",
+      photo:
+        "https://w7.pngwing.com/pngs/650/450/png-transparent-prothom-alo-%E0%A6%96%E0%A7%87%E0%A6%B2%E0%A6%BE-dhaka-bengali-sport-others-text-sport-logo.png",
+    },
+  },
+  {
+    id: 9,
+    p_content: `তিক্ত স্বাদ নিয়েই গত মাসে জুভেন্টাস ছেড়েছেন আনহেল দি মারিয়া। এক বছরের চুক্তিতে ইতালিয়ান ক্লাবটিতে যোগ দেওয়ার পর কোনো শিরোপা জিততে পারেননি। এরপর থেকেই তাঁকে ঘিরে গুঞ্জন শুরু হয়েছিল।
+
+		লিওনেল মেসি যে ক্লাবে যোগ দিচ্ছেন সেই ইন্টার মায়ামির পক্ষ থেকেও যোগাযোগ করা হয়েছিল তাঁর সঙ্গে। কিন্তু আর্জেন্টাইন তারকা ভেবে রেখেছিলেন অন্য কিছু। মায়ামির সহমালিক হোর্হে মাসের কথায়ও সেই ইঙ্গিত ছিল, ‘আমরা দি মারিয়ার সঙ্গে যোগাযোগ করেছি কিন্তু সে সম্ভবত অন্য কোনো ক্লাবে যোগ দেবে।’ তখনই প্রশ্ন উঠেছিল, দি মারিয়া কি তাহলে নিজের ইউরোপিয়ান শেকড়ে ফিরছেন?`,
+    p_media: {
+      photo:
+        "https://images.prothomalo.com/prothomalo-bangla%2F2023-07%2F8a7a7f3f-fb81-42e5-8518-c0cf55575400%2F344703530_1371853270416061_7314927641558623195_n.jpg?rect=0%2C0%2C992%2C558&auto=format%2Ccompress&fmt=webp&format=webp&w=900&dpr=1.3",
+    },
+
+    like_count: 450,
+    comment_count: 100,
+    post_time: "10 min",
+    user: {
+      name: "Prothom Alo",
+      photo:
+        "https://w7.pngwing.com/pngs/650/450/png-transparent-prothom-alo-%E0%A6%96%E0%A7%87%E0%A6%B2%E0%A6%BE-dhaka-bengali-sport-others-text-sport-logo.png",
+    },
+  },
+  {
+    id: 10,
+    p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+    p_media: {
+      photo: "",
+    },
+
+    like_count: 450,
+    comment_count: 100,
+    post_time: "10 min",
+    user: {
+      name: "Jasper AI",
+      photo:
+        "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+    },
+  },
+];
